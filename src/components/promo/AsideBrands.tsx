fd32a33134bccbd635bbc3a2c1e02b2bd6754011
Alexander <al.tulin@gmail.com>
2024-06-20 08:53:26 +0800

    fix err icon brands

--- src/components/promo/AsideBrands.tsx
+++ src/components/promo/AsideBrands.tsx
@@ -1,13 +1,16 @@
 import clsx from "clsx";
 import style from "./Promo.module.scss";
 import { FC } from "react";
-import { LazySvg } from "@/hoc/LazySvg";
+import IconBrand_1 from "@/images/promo/brand-1.svg?react";
+import IconBrand_2 from "@/images/promo/brand-2.svg?react";
+import IconBrand_3 from "@/images/promo/brand-3.svg?react";
+import IconBrand_4 from "@/images/promo/brand-4.svg?react";
 
 const brands = [
-  { href: "#", alt: "" },
-  { href: "#", alt: "" },
-  { href: "#", alt: "" },
-  { href: "#", alt: "" },
+  { href: "#", alt: "", icon: IconBrand_1 },
+  { href: "#", alt: "", icon: IconBrand_2 },
+  { href: "#", alt: "", icon: IconBrand_3 },
+  { href: "#", alt: "", icon: IconBrand_4 },
 ];
 
 const AsideBrands: FC = () => {
@@ -21,7 +24,8 @@ const AsideBrands: FC = () => {
               className={clsx(style.aside__link)}
               target="_blank"
             >
-              <LazySvg path={`../images/promo/brand-${i + 1}`} />
+              {/* <LazySvg path={`./assets/brand-${i + 1}`} /> */}
+              <item.icon />
             </a>
           </li>
         ))}
