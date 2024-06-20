f27be8005077c0d9e86f5219255cd089ab16830d
Alexander <al.tulin@gmail.com>
2024-06-20 17:07:23 +0800

    add speakers

--- src/components/program/Program.tsx
+++ src/components/program/Program.tsx
@@ -6,7 +6,16 @@ const Program: FC = () => {
   return (
     <section className={clsx(style.program)}>
       <div className={clsx(style.program__inner)}>
-        Program Component
+        <div className={clsx(style.program__main)}>
+          <h2 className={clsx(style.title)}>
+            <span>программа</span>
+            <span>фестиваля</span>
+          </h2>
+
+          <div className={clsx(style.picture)}>
+            <figurecl></figure>
+          </div>
+        </div>
       </div>
     </section>
   );
