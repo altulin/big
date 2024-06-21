4975c82fbc9a789f2813015762b4a8f3829df128
Alexander <al.tulin@gmail.com>
2024-06-21 12:18:18 +0800

    add animation main

--- src/components/promo/Slide_1.tsx
+++ src/components/promo/Slide_1.tsx
@@ -1,18 +1,24 @@
 import clsx from "clsx";
 import style from "./Promo.module.scss";
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { canvasCursor } from "@/service/canvas";
+import { useIsTabletDevice } from "@/hooks/IsSmallDevice";
 import IconMain from "@/images/header/logo.svg?react";
 
 const Slide_1: FC = () => {
+  const isTablet = useIsTabletDevice();
+
+  useEffect(() => {
+    if (isTablet) return;
+    canvasCursor();
+  }, [isTablet]);
+
   return (
     <div className={clsx(style.slide_1)}>
-      <figure className={clsx(style.slide_1__figure)}>
-        <IconMain />
+      <figure className={clsx(style.slide_1__figure, "js-logo-placemove")}>
+        {isTablet ? <IconMain /> : <canvas id="canvas-sm"></canvas>}
       </figure>
       <p className={clsx(style.slide_1__text)}>
-        {/* <span></span>
-        <span></span>
-        <span></span> */}
         фестиваль видеоиндустрии, презентующий лучшие работы и&nbsp;персональные
         достижения в коммерческой и социальной рекламе, музыкальных видео и
         анимации
---
+++ src/service/canvas.ts
@@ -0,0 +1,192 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const canvasCursor = (unmount = false) => {
+  const body = document.body as HTMLBodyElement;
+  const parentBlock = body.querySelector(
+    ".js-logo-placemove",
+  ) as HTMLDivElement;
+
+  const rem = +window
+    .getComputedStyle(document.body)
+    .getPropertyValue("font-size")
+    .slice(0, -2);
+
+  const canvasHeight = parentBlock.offsetHeight - 7 * rem;
+  const canvasWidth = parentBlock.offsetWidth - 2 * rem;
+  const img = new Image();
+  img.src = new URL("./assets/canvas_logo.svg", import.meta.url).href;
+
+  let canvas: HTMLCanvasElement | null = null;
+  let ctx: any;
+
+  let smallSquare: any;
+  let bigSquare: any;
+
+  const scale = window.devicePixelRatio;
+
+  if (body.querySelectorAll("#canvas-sm")?.length) {
+    canvas = body.querySelector("#canvas-sm") as HTMLCanvasElement;
+    ctx = canvas.getContext("2d");
+
+    if (devicePixelRatio >= 2) {
+      ctx?.scale(2, 2);
+    }
+
+    bigSquare = {
+      x1: 1,
+      y1: 1,
+      x2: 20,
+      y2: 11.6,
+      w: 19,
+      h: 10.6,
+    };
+    refreshSizeSm();
+  }
+
+  if (!canvas) return;
+  if (!ctx) return;
+
+  canvas.style.width = canvasWidth + "px";
+  canvas.style.height = canvasHeight + "px";
+  canvas.width = Math.floor(canvasWidth * scale);
+  canvas.height = Math.floor(canvasHeight * scale);
+  ctx.scale(scale, scale);
+
+  ctx.strokeStyle = "#11FF00";
+  ctx.lineWidth = 0.1 * rem;
+
+  ctx.imageSmoothingEnabled = false;
+
+  function refreshSizeSm() {
+    smallSquare = {
+      x1: Math.round(((canvasWidth / rem) * 50) / 100 - 5),
+      y1: Math.round(((canvasHeight / rem) * 50) / 100 - 2.5),
+      x2: Math.round(((canvasWidth / rem) * 50) / 100 + 5),
+      y2: Math.round(((canvasHeight / rem) * 50) / 100 + 2.5),
+    };
+  }
+
+  function _drawImage() {
+    const x1 = Math.round(bigSquare.x1 * rem + rem);
+    const y1 = Math.round(bigSquare.y1 * rem + rem);
+    const x2 = Math.round((bigSquare.w - 2) * rem);
+    const y2 = Math.round((bigSquare.h - 2) * rem);
+
+    img.onload = function () {
+      ctx.drawImage(img, x1, y1, x2, y2);
+    };
+
+    ctx.drawImage(img, x1, y1, x2, y2);
+  }
+
+  function drawBigSquare() {
+    ctx.beginPath();
+    ctx.moveTo(`${bigSquare.x1 * rem}`, `${bigSquare.y1 * rem}`);
+    ctx.lineTo(`${bigSquare.x2 * rem}`, `${bigSquare.y1 * rem}`);
+    ctx.lineTo(`${bigSquare.x2 * rem}`, `${bigSquare.y2 * rem}`);
+    ctx.lineTo(`${bigSquare.x1 * rem}`, `${bigSquare.y2 * rem}`);
+    ctx.lineTo(`${bigSquare.x1 * rem}`, `${bigSquare.y1 * rem}`);
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  function drawSmallSquare() {
+    ctx.beginPath();
+    ctx.moveTo(`${smallSquare.x1 * rem}`, `${smallSquare.y1 * rem}`);
+    ctx.lineTo(`${smallSquare.x2 * rem}`, `${smallSquare.y1 * rem}`);
+    ctx.lineTo(`${smallSquare.x2 * rem}`, `${smallSquare.y2 * rem}`);
+    ctx.lineTo(`${smallSquare.x1 * rem}`, `${smallSquare.y2 * rem}`);
+    ctx.lineTo(`${smallSquare.x1 * rem}`, `${smallSquare.y1 * rem}`);
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  function drawLine() {
+    ctx.beginPath();
+
+    ctx.moveTo(`${bigSquare.x1 * rem}`, `${bigSquare.y1 * rem}`);
+    ctx.lineTo(`${smallSquare.x1 * rem}`, `${smallSquare.y1 * rem}`);
+    ctx.stroke();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.moveTo(`${bigSquare.x2 * rem}`, `${bigSquare.y1 * rem}`);
+    ctx.lineTo(`${smallSquare.x2 * rem}`, `${smallSquare.y1 * rem}`);
+    ctx.stroke();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.moveTo(`${bigSquare.x2 * rem}`, `${bigSquare.y2 * rem}`);
+    ctx.lineTo(`${smallSquare.x2 * rem}`, `${smallSquare.y2 * rem}`);
+    ctx.stroke();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.moveTo(`${bigSquare.x1 * rem}`, `${bigSquare.y2 * rem}`);
+    ctx.lineTo(`${smallSquare.x1 * rem}`, `${smallSquare.y2 * rem}`);
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  draw();
+
+  function draw() {
+    drawLine();
+    drawBigSquare();
+    drawSmallSquare();
+    _drawImage();
+  }
+
+  function redraw(event: any) {
+    const canvasParam = (canvas as HTMLCanvasElement).getBoundingClientRect();
+    const minLeftX =
+      (event.clientX - canvasParam.left - (bigSquare.w / 2) * rem) / rem;
+    const minRightX = minLeftX + bigSquare.w;
+    const maxRightX = canvasParam.width / rem;
+    const minTopY =
+      (event.clientY - canvasParam.top - (bigSquare.h / 2) * rem) / rem;
+    const minBottomY = minTopY + bigSquare.h;
+    const maxBottomY = canvasParam.height / rem;
+
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    if (event.clientX > canvasParam.left + (bigSquare.w / 2) * rem) {
+      bigSquare.x1 = minLeftX;
+      bigSquare.x2 = minRightX;
+      if (event.clientX > canvasParam.right - (bigSquare.w / 2) * rem * 1.005) {
+        bigSquare.x2 = maxRightX / 1.0025;
+        bigSquare.x1 = bigSquare.x2 - bigSquare.w;
+      }
+    } else {
+      bigSquare.x1 = 0;
+      bigSquare.x2 = bigSquare.w;
+    }
+
+    if (event.clientY > canvasParam.top + (bigSquare.h / 2) * rem) {
+      bigSquare.y1 = minTopY;
+      bigSquare.y2 = minBottomY;
+      if (
+        event.clientY >
+        canvasParam.bottom - (bigSquare.h / 2) * rem * 1.005
+      ) {
+        bigSquare.y2 = maxBottomY / 1.0025;
+        bigSquare.y1 = bigSquare.y2 - bigSquare.h;
+      }
+    } else {
+      bigSquare.y1 = 0;
+      bigSquare.y2 = bigSquare.h;
+    }
+  }
+
+  const handleDraw = (e: any) => {
+    redraw(e);
+    draw();
+  };
+
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
+      navigator.userAgent,
+    )
+  ) {
+    return;
+  } else {
+    body.addEventListener("mousemove", handleDraw);
+  }
+
+  if (unmount) body.removeEventListener("mousemove", handleDraw);
+};
