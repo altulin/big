9e82eb29fbc94ebc32a2ab59de42891ce539818b
Alexander <al.tulin@gmail.com>
2024-09-19 17:38:24 +0800

    fix

--- src/components/short_list/Shortlist.tsx
+++ src/components/short_list/Shortlist.tsx
@@ -46,20 +46,17 @@ const Shortlist: FC = () => {
                 isSuccessNominations &&
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 data.results.map((el: any, i: number) => (
-                  <>
-                    <h3>fgh</h3>
-                    <ShortRow
-                      key={i}
-                      nomination={
-                        dataNominations.filter(
-                          (m) => m.value === el.nomination,
-                        )[0].label
-                      }
-                      name_work={el.title}
-                      author="Петр Петров"
-                      id={el.id}
-                    />
-                  </>
+                  <ShortRow
+                    key={i}
+                    nomination={
+                      dataNominations.filter(
+                        (m) => m.value === el.nomination,
+                      )[0].label
+                    }
+                    name_work={el.title}
+                    author="Петр Петров"
+                    id={el.id}
+                  />
                 ))}
             </ul>
           </ScrollBarComponent>
