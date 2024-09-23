b6de24c45d2c3966824ec17d0d6b77480386d5e4
Alexander <al.tulin@gmail.com>
2024-09-23 15:21:13 +0800

    delete slide brand

---
+++ src/components/winners/winners.tsx
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+import style from "./Winners.module.scss";
+import { FC } from "react";
+
+const Winners: FC = () => {
+  return (
+    <section className={clsx(style.winners)}>
+      <div className={clsx(style.winners__inner)}>
+        Winners Component
+      </div>
+    </section>
+  );
+};
+
+export default Winners;
