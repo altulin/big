395ad51e202cf53039939a03342520b4c2ba6a1c
Alexander <al.tulin@gmail.com>
2024-09-30 10:49:57 +0800

    rm link winners

--- src/components/winners/VideoLink.tsx
+++ src/components/winners/VideoLink.tsx
@@ -10,25 +10,30 @@ const WinnersLink: FC<IWinnersMedia> = ({
 }) => {
   return (
     <div className={clsx(style.link, !image_url && style["link--image"])}>
-      <a
-        href={link}
-        className={clsx(
-          style.control_link,
-          image_url && style["control_link--image"],
-        )}
-        target="_blank"
-      >
-        {!image_url && (
-          <>
-            <span className={clsx(style.control_link__icon)}>
-              <IconPlayLink />
-            </span>
-            <span className={clsx(style.control_link__content)}>
-              <span className={clsx(style.control_link__label)}>{link}</span>
-            </span>
-          </>
-        )}
-      </a>
+      {image_url ? (
+        <div
+          className={clsx(
+            style.control_link,
+            image_url && style["control_link--image"],
+          )}
+        ></div>
+      ) : (
+        <a
+          href={link}
+          className={clsx(
+            style.control_link,
+            image_url && style["control_link--image"],
+          )}
+          target="_blank"
+        >
+          <span className={clsx(style.control_link__icon)}>
+            <IconPlayLink />
+          </span>
+          <span className={clsx(style.control_link__content)}>
+            <span className={clsx(style.control_link__label)}>{link}</span>
+          </span>
+        </a>
+      )}
     </div>
   );
 };
